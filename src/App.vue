e55c609b4c1a3bc0bd24ecf5ee0ac52fb2be5cdf
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2024-11-21 21:06:05 +0100

    test suspens

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router/auto'
 import LogInOut from './components/LogInOut.vue'
-
-import {  onMounted } from 'vue'
+import { onMounted } from 'vue'
 import { initCustomCursor } from './assets/custom-cursor'
-
 import HeaderPage from './components/HeaderPage.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
 
 onMounted(() => {
   console.log('Vue app mounted')
   initCustomCursor()
 })
-
 </script>
 
 <template>
@@ -19,7 +17,12 @@ onMounted(() => {
   <main>
     <RouterView v-slot="{ Component }">
       <Suspense>
-        <component :is="Component" :key="$route.path" />
+        <template #default>
+          <component :is="Component" :key="$route.path" />
+        </template>
+        <template #fallback>
+          <LoadingSpinner />
+        </template>
       </Suspense>
     </RouterView>
   </main>
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading-spinner">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<style scoped>
+.loading-spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: black
+}
+
+.spinner {
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-left-color: #fff;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
