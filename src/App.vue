d31b99899ce88418f98338e8b92623ac3ed85e24
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2024-12-09 18:15:15 +0100

    essai suspense app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,31 @@ import { RouterLink, RouterView } from 'vue-router/auto';
 import LogInOut from './components/LogInOut.vue';
 import { onMounted } from 'vue';
 import { initCustomCursor } from './assets/custom-cursor';
-import HeaderPage from './components/HeaderPage.vue';
+// import HeaderPage from './components/HeaderPage.vue';
 import LoadingSpinner from './components/LoadingSpinner.vue';
+
+import { defineAsyncComponent } from 'vue'
 
 onMounted(() => {
   console.log('Vue app mounted');
   initCustomCursor();
 });
+
+const HeaderPage = defineAsyncComponent(() => import('@/components/HeaderPage.vue'))
 </script>
 
 <template>
   <HeaderPage />
-  <main>
-    <RouterView v-slot="{ Component }">
-      <Suspense>
-        <template #default>
+  <Suspense>
+    <template #default>
+      <main>
+        <RouterView v-slot="{ Component }">
           <component :is="Component" :key="$route.path" />
-        </template>
-        <template #fallback>
-          <LoadingSpinner />
-        </template>
-      </Suspense>
-    </RouterView>
-  </main>
+        </RouterView>
+      </main>
+    </template>
+    <template #fallback>
+      <LoadingSpinner />
+    </template>
+  </Suspense>
 </template>
